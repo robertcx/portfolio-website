820b2f5fe81126d0e2d65b85df2e2f72348dcb6f
Robert <69506653+robertcx@users.noreply.github.com>
2022-10-12 00:41:00 +0300

    Finished hero section

--- app.js
+++ app.js
@@ -12,37 +12,31 @@
 });
 
 */
-document.addEventListener("DOMContentLoaded", function(){
-
+document.addEventListener('DOMContentLoaded', function () {
   el_autohide = document.querySelector('.autohide');
-  
+
   navbar_height = document.querySelector('.navbar').offsetHeight;
 
-  if(el_autohide){
+  if (el_autohide) {
     var last_scroll_top = 0;
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
       let scroll_top = window.scrollY;
 
-      if(window.scrollY > 100) {
-        $('.navbar').css('background', 'rgba(10,10,10,1)');
+      if (window.scrollY > 100) {
+        $('.navbar').css('background', 'rgba(10,10,10,0.8)');
 
-        if(scroll_top < last_scroll_top) {
-            el_autohide.classList.remove('scrolled-down');
-            el_autohide.classList.add('scrolled-up');
-        }
-        else {
-            el_autohide.classList.remove('scrolled-up');
-            el_autohide.classList.add('scrolled-down');
+        if (scroll_top < last_scroll_top) {
+          el_autohide.classList.remove('scrolled-down');
+          el_autohide.classList.add('scrolled-up');
+        } else {
+          el_autohide.classList.remove('scrolled-up');
+          el_autohide.classList.add('scrolled-down');
         }
 
         last_scroll_top = scroll_top;
-
-      }
-      else {
+      } else {
         $('.navbar').css('background', 'rgba(0,0,0,0)');
       }
-
-    }); 
+    });
   }
-
 });
