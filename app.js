c4880921b6fdc34cab362c0551ce866e0065cf67
Robert <69506653+robertcx@users.noreply.github.com>
2022-10-14 00:01:48 +0300

    Blur navbar

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
+$(document).scroll(function () {
+  var navbarHeight = $('.navbar').height();
+  var alpha = $(this).scrollTop() / navbarHeight / 10;
+  if (alpha > 0.4) {
+    $('.navbar').css(
+      'background-image',
+      'linear-gradient(to bottom, rgba(10,10,10,1), rgba(0,0,0,0)'
+    );
+  } else {
+    $('.navbar').css(
+      'background-image',
+      'linear-gradient(to bottom, rgba(10,10,10,' +
+        alpha * 4 +
+        '), rgba(0,0,0,0)'
+    );
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
-  el_autohide = document.querySelector('.autohide');
+  navbar_hide = document.querySelector('.autohide');
 
   navbar_height = document.querySelector('.navbar').offsetHeight;
 
-  if (el_autohide) {
+  if (navbar_hide) {
     var last_scroll_top = 0;
     window.addEventListener('scroll', function () {
       let scroll_top = window.scrollY;
 
-      if (window.scrollY > 100) {
-        $('.navbar').css('background', 'rgba(10,10,10,0.8)');
-
+      if (window.scrollY > 400) {
         if (scroll_top < last_scroll_top) {
-          el_autohide.classList.remove('scrolled-down');
-          el_autohide.classList.add('scrolled-up');
+          navbar_hide.classList.remove('scrolled-down');
+          navbar_hide.classList.add('scrolled-up');
         } else {
-          el_autohide.classList.remove('scrolled-up');
-          el_autohide.classList.add('scrolled-down');
+          navbar_hide.classList.remove('scrolled-up');
+          navbar_hide.classList.add('scrolled-down');
         }
 
         last_scroll_top = scroll_top;
-      } else {
-        $('.navbar').css('background', 'rgba(0,0,0,0)');
       }
     });
   }
